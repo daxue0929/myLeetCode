4237a9726e8af49411481a0297cdf0c01110fd5d
wangxuedi <wangxuedi@izton.com>
2020-08-13 14:06:06 +0800

    addone

---
+++ partone/src/main/java/com/daxue/AddOne.java
@@ -0,0 +1,38 @@
+package com.daxue;
+
+import sun.java2d.windows.GDIWindowSurfaceData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AddOne {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] origin ={0,0,1,1,1,2,2,3,3,4}; // 示例数据
+        int[] result = solution.plusOne(origin);
+        System.out.println(result);
+    }
+
+    /**
+     * 对于长的数组, 运算有越界的的异常错误
+     * version: 1.0
+     */
+    static class Solution {
+        public int[] plusOne(int[] digits) {
+            ArrayList<Integer> list = new ArrayList<>();
+            String str = "";
+            for (int i=0; i<digits.length; i++) {
+                list.add(digits[i]);
+                str += digits[i];
+            }
+            int resultNUmber = Integer.parseInt(str) + 1;
+
+            int [] result = new int[String.valueOf(resultNUmber).length()];
+            for (int i = String.valueOf(resultNUmber).length()-1; i >=0; i--) {
+                result[i] = resultNUmber % 10;
+                resultNUmber = resultNUmber / 10;
+            }
+            return result;
+        }
+    }
+}
